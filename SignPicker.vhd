8d603ec7dd69fc34fe169d1b7d65f315993c50c4
marturiel00 <100971860+marturiel00@users.noreply.github.com>
2023-04-19 17:06:59 +0200

    Update SignPicker.vhd
    fix typo

--- SignPicker.vhd
+++ SignPicker.vhd
@@ -9,7 +9,7 @@ port(
 	E_comp : in std_logic_vector(1 downto 0); --10 is equal, 01 is grt, sml bit is ignored since 00 implies sml
 	M_comp : in std_logic; --greater or not  
 	S_grt  : out std_logic);
-end SignComparator;
+end SignPicker;
 
 
 architecture RTL of SignPicker is
