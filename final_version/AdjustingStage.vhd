c1f5fcefd6d43162ffc231b30521c71ebca20321
Maurizio Tirabassi <106387310+MaurizioTirabassi@users.noreply.github.com>
2023-07-02 10:10:10 +0200

    Update AdjustingStage.vhd

--- final_version/AdjustingStage.vhd
+++ final_version/AdjustingStage.vhd
@@ -70,7 +70,7 @@ begin
 		);
 		
 	--Checking for the underflow of the corrected exponent
-	TMP_ERR <= "011" when TMP_UF = '1' else ERR; --TODO: ora NaN, capire che caso è
+	TMP_ERR <= "011" when (TMP_UF = '1' and ERR = "000") else ERR; --TODO: ora NaN, capire che caso è
 
 	--Translating the encoded special output signal ERR and eventually signaling the abnormality
 	SPCL:	SpecialOutput
